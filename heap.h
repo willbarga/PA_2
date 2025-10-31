523c6d6e24fdec166a7736a4265814702f6287f6
willbarga <wbarga8657@sdsu.edu>
2025-10-31 13:16:39 -0700

    Finished heap - William Barga

--- heap.h
+++ heap.h
@@ -36,10 +36,39 @@ struct MinHeap {
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+        while (pos > 0) {
+            int parent = (pos - 1) / 2;
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+                swap(data[pos], data[parent]);
+                pos = parent;
+            }
+            else {
+                break;
+            }
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
+        while (true) {
+            int left = 2 * pos + 1;
+            int right = 2 * pos + 2;
+            int smallest = pos;
+
+            if (left < size && weightArr[data[left]] < weightArr[data[smallest]]) {
+                smallest = left;
+            }
+            if (right < size && weightArr[data[right]] < weightArr[data[smallest]]) {
+                smallest = right;
+            }
+            if (smallest != pos) {
+                swap(data[pos], data[smallest]);
+                pos = smallest;
+            }
+            else {
+                break;
+            }
+        }
     }
 };
 
